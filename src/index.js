0381b3001dbea298c5517072d6a5dfc7428a0845
rushichaudhari <53929994+rushichaudhari@users.noreply.github.com>
2021-02-27 14:12:53 -0500

    not responsive search results

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import './index.css';
 import App from './App';
+import ProductVisibility from './components/product-visibility';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App/> */}
+    <ProductVisibility />
   </React.StrictMode>,
   document.getElementById('root')
 );
