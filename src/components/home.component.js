716b490b1272bb78b7b1dda9fda16c128793e4fe
kurkures96 <kurkure.s@husky.neu.edu>
2021-02-27 01:26:39 -0500

    Home Component Minor Changes

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -7,12 +6,10 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import {makeStyles} from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import {Link} from "react-router-dom";
-import HomeWorkIcon from '@material-ui/icons/HomeWork';
 import SearchAppBar from "./search-bar.component";
 
 function Copyright() {
@@ -66,7 +63,7 @@ export default function Home() {
     const classes = useStyles();
 
     return (
-        <React.Fragment>
+        <div>
             <CssBaseline/>
             <div>
                 <SearchAppBar/>
@@ -145,6 +142,6 @@ export default function Home() {
                 <Copyright/>
             </footer>
             {/* End footer */}
-        </React.Fragment>
+        </div>
     );
 }
