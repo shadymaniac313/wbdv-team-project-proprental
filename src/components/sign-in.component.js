a31de5cc5b982d77055534c517d84a50afb214dc
kurkures96 <kurkure.s@husky.neu.edu>
2021-02-26 23:57:26 -0500

    Home Page Added

--- src/components/sign-in.component.js
+++ src/components/sign-in.component.js
@@ -3,8 +3,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import {Link} from "react-router-dom";
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
@@ -12,11 +10,14 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import Toolbar from "@material-ui/core/Toolbar";
+import HomeWorkIcon from "@material-ui/icons/HomeWork";
+import AppBar from "@material-ui/core/AppBar";
 
 
 const useStyles = makeStyles((theme) => ({
     paper: {
-        marginTop: theme.spacing(8),
+        marginTop: theme.spacing(10),
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
@@ -40,9 +41,15 @@ export default function SignIn() {
     return (
         <Container component="main" maxWidth="xs">
 
-            <Typography className={classes.paper} component="h1" variant="h3">
-                Property Listing
-            </Typography>
+            <AppBar>
+                <Toolbar>
+                    <HomeWorkIcon className={classes.icon}/>
+                    &nbsp; &nbsp;
+                    <Typography variant="h6" color="inherit" noWrap>
+                        Property Listing
+                    </Typography>
+                </Toolbar>
+            </AppBar>
 
             <CssBaseline />
             <div className={classes.paper}>
