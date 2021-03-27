9ced5952e745d9bd38ecaaf0acae14233f36f03b
kurkures96 <kurkure.s@husky.neu.edu>
2021-03-27 00:05:07 -0400

    service working, need to send the state from one component to another

--- src/services/search-service.js
+++ src/services/search-service.js
@@ -3,3 +3,10 @@ const API_URL = `https://api.bridgedataoutput.com/api/v2/test/listings?access_to
 
 export const findPropertyByCity = (cityName) =>
   fetch(API_URL + `&City.in=${cityName}`).then((response) => response.json());
+  // fetch(
+  //   "https://api.bridgedataoutput.com/api/v2/test/listings?access_token=4e6582796dec50bbfda69f32fc04a818"
+  // ).then((response) => response.json());
+
+export default {
+  findPropertyByCity,
+};
