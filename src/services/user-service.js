9bec834880a8ea40c27dc8b35e4220fb7a18449d
kurkures96 <kurkure.s@husky.neu.edu>
2021-04-15 00:40:59 -0400

    sign up works

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -6,13 +6,38 @@ export const signInService = async (username, password) => {
   ).then((response) => response.json());
 };
 
+// export const signUpService = async (
+//   firstName,
+//   lastName,
+//   username,
+//   password
+// ) => {
+//   return true;
+// };
+
 export const signUpService = async (
   firstName,
   lastName,
   username,
   password
 ) => {
-  return true;
+  const newUser = {
+    firstName: firstName,
+    lastName: lastName,
+    phone: "12345",
+    userType: 1,
+    pwd: password,
+    username: username,
+  };
+
+  const response = await fetch(`${LOCAL_HOST}users/create`, {
+    method: "POST",
+    body: JSON.stringify(newUser),
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+  return response.ok;
 };
 
 const userService = {
