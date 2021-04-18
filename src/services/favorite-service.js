56844da1a348cca03b312e5a7321c3df2e91bd24
kurkures96 <kurkure.s@husky.neu.edu>
2021-04-17 22:07:08 -0400

    favorite-service.js added

---
+++ src/services/favorite-service.js
@@ -0,0 +1,39 @@
+const LOCAL_HOST = "http://localhost:8080/api/";
+
+export const getFavListing = async (userId) => {
+  return fetch(`${LOCAL_HOST}listing/user/${userId}`).then((response) =>
+    response.json()
+  );
+};
+
+export const postFavListing = async (userId, listingId) => {
+  const newListing = {
+    userId: userId,
+    listingId: listingId,
+  };
+
+  const response = await fetch(`${LOCAL_HOST}users/like`, {
+    method: "POST",
+    body: JSON.stringify(newListing),
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+  return response.ok;
+};
+
+export const postUnFavListing = async (userId, listingId) => {
+  const newListing = {
+    userId: userId,
+    listingId: listingId,
+  };
+
+  const response = await fetch(`${LOCAL_HOST}users/unlike`, {
+    method: "POST",
+    body: JSON.stringify(newListing),
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+  return response.ok;
+};
