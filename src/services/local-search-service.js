c200dc62d9bdbb0f4c8d8e5488385a71914a983d
Neel Deshpande <deshpande.nee@husky.neu.edu>
2021-04-28 15:29:50 -0400

    price for local properties

--- src/services/local-search-service.js
+++ src/services/local-search-service.js
@@ -1,33 +1,37 @@
 const LOCAL_HOST = "http://localhost:8080/api/";
 
 export const findParcelByState = (cityObject) => {
-  return fetch(
-    `${LOCAL_HOST}properties/state/${cityObject.city}`
-  ).then((response) => response.json());
+    return fetch(
+        `${LOCAL_HOST}properties/state/${cityObject.city}`
+    ).then((response) => response.json());
 };
 
 export const findParcelById = (Object) => {
-  return fetch(`${LOCAL_HOST}properties/${Object.ListingId}`).then((response) =>
-    response.json()
-  );
+    return fetch(`${LOCAL_HOST}properties/${Object.ListingId}`).then((response) =>
+        response.json()
+    );
 };
 
 export const findAgentId = (Object) => {
-  return fetch(`${LOCAL_HOST}listings/${Object.ListingId}`).then((response) =>
-    response.json()
-  );
+    return fetch(`${LOCAL_HOST}listings/${Object.ListingId}`).then((response) =>
+        response.json()
+    );
 };
 
+export const findPriceByListingId = (listingId) => fetch(`${LOCAL_HOST}listings/${listingId}`)
+    .then(price => price.json())
+
 export const findPropertiesByListing = (Object) => {
-  return fetch(
-    `${LOCAL_HOST}properties/listing/${Object.ListingId}`
-  ).then((response) => response.json());
+    return fetch(
+        `${LOCAL_HOST}properties/listing/${Object.ListingId}`
+    ).then((response) => response.json());
 };
 
 const localSearchService = {
-  findParcelByState,
-  findParcelById,
-  findAgentId,
-  findPropertiesByListing,
+    findParcelByState,
+    findParcelById,
+    findAgentId,
+    findPropertiesByListing,
+    findPriceByListingId
 };
 export default localSearchService;
